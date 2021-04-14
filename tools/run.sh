3f16f76e0b1f9d0300d4e622905c8d16bc079f93
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-04-14 19:36:26 -0300

    CMake/Tools: Added run.sh

---
+++ tools/run.sh
@@ -0,0 +1,5 @@
+[ -z "$QEMU_BIN" ] && QEMU_BIN="qemu-system-i386"
+[ -z "$QEMU_ARG" ] && QEMU_ARG="-kernel kernel/kernel"
+[ -e /dev/kvm ] && [ -r /dev/kvm ] && [ -w /dev/kvm ] && QEMU_KVM="-enable-kvm"
+
+$QEMU_BIN $QEMU_ARG $QEMU_KVM
