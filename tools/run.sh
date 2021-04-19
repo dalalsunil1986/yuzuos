b756cf755e37127e0b10883e4db0be77e664f0f1
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-04-19 16:56:28 -0300

    Tools: Enabled qemu serial to stdio output

--- tools/run.sh
+++ tools/run.sh
@@ -1,5 +1,5 @@
 [ -z "$QEMU_BIN" ] && QEMU_BIN="qemu-system-i386"
-[ -z "$QEMU_ARG" ] && QEMU_ARG="-kernel kernel/kernel"
+[ -z "$QEMU_ARG" ] && QEMU_ARG="-kernel kernel/kernel -serial stdio"
 [ -e /dev/kvm ] && [ -r /dev/kvm ] && [ -w /dev/kvm ] && QEMU_KVM="-enable-kvm"
 
 $QEMU_BIN $QEMU_ARG $QEMU_KVM
