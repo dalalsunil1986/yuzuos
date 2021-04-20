93438fad8ba43961239e8f60af235f5fdfe2d8a6
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-04-20 16:22:52 -0300

    Kernel: Added IDT

--- kernel/init.c
+++ kernel/init.c
@@ -1,11 +1,13 @@
 #include <kernel/utils/log.h>
 #include <kernel/memory/gdt.h>
+#include <kernel/interrupts/idt.h>
 #include <stdbool.h>
 
 void kernel_init()
 {
   log_init();
   gdt_init();
+  idt_init();
 
   while (true)
     ;
