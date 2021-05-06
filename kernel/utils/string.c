92f00cbc303ef270c024d35f3b04af186d9f3563
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-05-06 16:32:48 -0300

    Kernel: Added memset

---
+++ kernel/utils/string.c
@@ -0,0 +1,9 @@
+#include <kernel/utils/string.h>
+
+void *memset(void *str, int c, size_t n)
+{
+  char *p_str = (char *)str;
+  for (size_t i = 0; i < n; i++)
+    p_str[i] = (unsigned char)c;
+  return str;
+}
