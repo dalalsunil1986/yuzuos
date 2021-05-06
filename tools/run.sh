b07ed0755c7daa951ebd31f9bb7bbcbac457c519
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-05-06 17:16:52 -0300

    CMake: Send QEMU bin to run target

--- tools/run.sh
+++ tools/run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -e
+
 [ -z "$QEMU_BIN" ] && QEMU_BIN="qemu-system-i386"
 [ -z "$QEMU_ARG" ] && QEMU_ARG="-kernel kernel/kernel -serial stdio"
 [ -e /dev/kvm ] && [ -r /dev/kvm ] && [ -w /dev/kvm ] && QEMU_KVM="-enable-kvm"
