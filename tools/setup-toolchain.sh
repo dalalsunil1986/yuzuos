168dfa0005ea61f57aa9e01e907279da1d2254d3
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-05-10 13:38:58 -0300

    Tools: We now have a specific toolchain Reverted Binutils: 2.36.1 => 2.35.1 Reverted GCC: 11.1.0 => 10.2.0

--- tools/setup-toolchain.sh
+++ tools/setup-toolchain.sh
@@ -4,17 +4,18 @@ TOOLS_DIR=$(pwd)/tools
 CROSS_DIR=$TOOLS_DIR/cross
 BUILD_DIR=$TOOLS_DIR/build
 CACHE_DIR=$TOOLS_DIR/cache
+PATCHES_DIR=$SOURCE_DIR/tools/patches
 
 CORES=$(nproc)
 PREFIX=$CROSS_DIR
-TARGET=i686-elf
+TARGET=i686-yuzuos
 MIRROR=https://ftp.gnu.org/gnu
 
-BINUTILS_VERSION=2.36.1
+BINUTILS_VERSION=2.35.1
 BINUTILS_PACKAGE=binutils-$BINUTILS_VERSION
 BINUTILS=$BINUTILS_PACKAGE.tar.xz
 
-GCC_VERSION=11.1.0
+GCC_VERSION=10.2.0
 GCC_PACKAGE=gcc-$GCC_VERSION
 GCC=$GCC_PACKAGE.tar.xz
 
@@ -32,6 +33,9 @@ function cleanup {
 trap cleanup EXIT
 
 mkdir -pv $BUILD_DIR $CACHE_DIR $CROSS_DIR
+mkdir -pv $SYSROOT_DIR $SYSROOT_DIR/usr/include
+
+cp -RT $SOURCE_DIR/libs/c/include $SYSROOT_DIR/usr/include
 
 cd $CACHE_DIR
 wget -nc $MIRROR/binutils/$BINUTILS
@@ -40,16 +44,32 @@ wget -nc $MIRROR/gcc/$GCC_PACKAGE/$GCC
 if [ ! -d $BUILD_DIR/$BINUTILS_PACKAGE ]; then
   cd $BUILD_DIR
   tar -xf $CACHE_DIR/$BINUTILS
+
+  cd $BUILD_DIR/$BINUTILS_PACKAGE
+  git init
+  git add .
+  git commit -m 'Initial commit'
+  git apply --stat $PATCHES_DIR/binutils.patch
+  git apply --check $PATCHES_DIR/binutils.patch
+  git am --signoff < $PATCHES_DIR/binutils.patch 
 fi
 
 if [ ! -d $BUILD_DIR/$GCC_PACKAGE ]; then
   cd $BUILD_DIR
   tar -xf $CACHE_DIR/$GCC
+
+  cd $BUILD_DIR/$GCC_PACKAGE
+  git init
+  git add .
+  git commit -m 'Initial commit'
+  git apply --stat $PATCHES_DIR/gcc.patch
+  git apply --check $PATCHES_DIR/gcc.patch
+  git am --signoff < $PATCHES_DIR/gcc.patch 
 fi
 
 mkdir $BUILD_DIR/binutils
 cd $BUILD_DIR/binutils
-../$BINUTILS_PACKAGE/configure --target=$TARGET --prefix=$PREFIX --with-sysroot --disable-nls --disable-werror
+../$BINUTILS_PACKAGE/configure --target=$TARGET --prefix=$PREFIX --with-sysroot=$SYSROOT_DIR --disable-nls --disable-werror
 make -j $CORES
 make install
 
@@ -58,6 +78,6 @@ cd $BUILD_DIR/$GCC_PACKAGE
  
 mkdir $BUILD_DIR/gcc
 cd $BUILD_DIR/gcc
-../$GCC_PACKAGE/configure --target=$TARGET --prefix=$PREFIX --disable-nls --enable-languages=c --without-headers
+../$GCC_PACKAGE/configure --target=$TARGET --prefix=$PREFIX --with-sysroot=$SYSROOT_DIR --disable-nls --enable-languages=c
 make -j $CORES all-gcc all-target-libgcc
 make install-gcc install-target-libgcc
