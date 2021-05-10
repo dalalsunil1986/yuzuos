42a0e5dd3cd21ff6d37f3dd9b33abf7c46a5de03
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-05-10 18:48:53 -0300

    Tools: Changed -drive arg on run.sh

--- tools/run.sh
+++ tools/run.sh
@@ -2,7 +2,7 @@
 
 [ -z "$YUZUOS_QEMU_BIN" ] && YUZUOS_QEMU_BIN="qemu-system-i386"
 [ -z "$YUZUOS_QEMU_IMG" ] && YUZUOS_QEMU_IMG="qemu_img"
-[ -z "$YUZUOS_QEMU_ARG" ] && YUZUOS_QEMU_ARG="-kernel kernel/kernel -serial stdio -drive file=${YUZUOS_QEMU_IMG},id=disk,if=none"
+[ -z "$YUZUOS_QEMU_ARG" ] && YUZUOS_QEMU_ARG="-kernel kernel/kernel -serial stdio -drive file=${YUZUOS_QEMU_IMG},format=raw,index=0,media=disk"
 [ -e /dev/kvm ] && [ -r /dev/kvm ] && [ -w /dev/kvm ] && QEMU_KVM="-enable-kvm"
 
 $YUZUOS_QEMU_BIN $YUZUOS_QEMU_ARG $QEMU_KVM
