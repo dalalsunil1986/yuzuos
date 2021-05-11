204f9448f1934a758b5fa4e5c6fcfb7dc6861614
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-05-11 10:28:17 -0300

    Github/Tools: Added support for github actions
    Generate toolchain cache on setup-toolchain.sh
    Added .github/workflows/cmake.yml

--- tools/setup-toolchain.sh
+++ tools/setup-toolchain.sh
@@ -6,6 +6,7 @@ BUILD_DIR=$TOOLS_DIR/build
 CACHE_DIR=$TOOLS_DIR/cache
 PATCHES_DIR=$SOURCE_DIR/tools/patches
 
+CACHE_FILE="toolchain_github_actions.tar.gz"
 CORES=$(nproc)
 PREFIX=$CROSS_DIR
 TARGET=i686-yuzuos
@@ -35,6 +36,27 @@ trap cleanup EXIT
 mkdir -pv $BUILD_DIR $CACHE_DIR $CROSS_DIR
 mkdir -pv $SYSROOT_DIR $SYSROOT_DIR/usr/include
 
+if [ "$TOOLCHAIN_CACHE" = "yes" ] ; then
+  if [ -r "$CACHE_DIR/$CACHE_FILE" ] ; then
+    echo "$CACHE_FILE exists"
+    
+    cd $CROSS_DIR
+    if tar xzf "$CACHE_DIR/$CACHE_FILE" ; then
+      echo "Cache extracted"
+      exit 0
+    else
+      echo ""
+      echo "Could not extract cached toolchain"
+      echo "This means the cache is broken"
+      echo ""
+
+      rm -f "$CACHE_FILE/$CACHE_FILE"
+    fi
+  else
+    echo "$CACHE_FILE does not exist"
+  fi
+fi
+
 cp -RT $SOURCE_DIR/libs/c/include $SYSROOT_DIR/usr/include
 
 cd $CACHE_DIR
@@ -46,12 +68,10 @@ if [ ! -d $BUILD_DIR/$BINUTILS_PACKAGE ]; then
   tar -xf $CACHE_DIR/$BINUTILS
 
   cd $BUILD_DIR/$BINUTILS_PACKAGE
-  git init
-  git add .
-  git commit -m 'Initial commit'
-  git apply --stat $PATCHES_DIR/binutils.patch
-  git apply --check $PATCHES_DIR/binutils.patch
-  git am --signoff < $PATCHES_DIR/binutils.patch 
+  git init > /dev/null
+  git add . > /dev/null
+  git commit -m 'Initial commit' > /dev/null
+  git apply $PATCHES_DIR/binutils.patch > /dev/null
 fi
 
 if [ ! -d $BUILD_DIR/$GCC_PACKAGE ]; then
@@ -59,12 +79,10 @@ if [ ! -d $BUILD_DIR/$GCC_PACKAGE ]; then
   tar -xf $CACHE_DIR/$GCC
 
   cd $BUILD_DIR/$GCC_PACKAGE
-  git init
-  git add .
-  git commit -m 'Initial commit'
-  git apply --stat $PATCHES_DIR/gcc.patch
-  git apply --check $PATCHES_DIR/gcc.patch
-  git am --signoff < $PATCHES_DIR/gcc.patch 
+  git init > /dev/null
+  git add . > /dev/null
+  git commit -m 'Initial commit' > /dev/null
+  git apply $PATCHES_DIR/gcc.patch > /dev/null
 fi
 
 mkdir $BUILD_DIR/binutils
@@ -81,3 +99,9 @@ cd $BUILD_DIR/gcc
 ../$GCC_PACKAGE/configure --target=$TARGET --prefix=$PREFIX --with-sysroot=$SYSROOT_DIR --disable-nls --enable-languages=c
 make -j $CORES all-gcc all-target-libgcc
 make install-gcc install-target-libgcc
+
+if [ "$TOOLCHAIN_CACHE" = "yes" ] ; then
+  cd $CACHE_DIR
+  rm -f $CACHE_FILE
+  tar -czf $CACHE_FILE -C $CROSS_DIR $(ls -A $CROSS_DIR)
+fi
