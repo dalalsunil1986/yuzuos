8a367065e63f36a3011618723b1ec8ea30be1375
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-05-11 15:40:13 -0300

    Kernel: Added more types on utils/types.h

--- kernel/include/kernel/utils/types.h
+++ kernel/include/kernel/utils/types.h
@@ -1,3 +1,9 @@
 #pragma once
 
 typedef unsigned long sector_t;
+typedef unsigned long long ino_t;
+typedef unsigned short umode_t;
+typedef unsigned int gid_t;
+typedef unsigned int uid_t;
+typedef unsigned int nlink_t;
+typedef signed long time_t;
