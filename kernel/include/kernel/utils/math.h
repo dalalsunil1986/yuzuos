bcc4164ad95170521026975a0081a3d839745e8f
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-05-12 15:01:33 -0300

    Kernel: Added MIN, MAX on math.h

--- kernel/include/kernel/utils/math.h
+++ kernel/include/kernel/utils/math.h
@@ -22,3 +22,19 @@
       type __y = (y);        \
       __x > __y ? __x : __y; \
     })
+
+#define MIN(x, y) (       \
+    {                     \
+      typeof(x) _x = (x); \
+      typeof(y) _y = (y); \
+      (void)(&_x == &_y); \
+      _x < _y ? _x : _y;  \
+    })
+
+#define MAX(x, y) (       \
+    {                     \
+      typeof(x) _x = (x); \
+      typeof(y) _y = (y); \
+      (void)(&_x == &_y); \
+      _x > _y ? _x : _y;  \
+    })
