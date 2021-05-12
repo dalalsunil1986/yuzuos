ad1e13da8845bff15ff17f7b57fe24f8fcd016f0
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-05-12 17:46:48 -0300

    Kernel: Added syscall_exit

--- kernel/include/kernel/system/syscall.h
+++ kernel/include/kernel/system/syscall.h
@@ -1,3 +1,5 @@
 #pragma once
 
+#include <ukernel/syscall.h>
+
 void syscall_init();
--- kernel/system/syscall.c
+++ kernel/system/syscall.c
@@ -1,10 +1,29 @@
 #include <kernel/system/syscall.h>
+#include <kernel/task/scheduler.h>
 #include <kernel/interrupts/isr.h>
 #include <kernel/utils/log.h>
+#include <kernel/utils/string.h>
+
+void syscall_exit(int32_t code)
+{
+  sched_exit(code);
+}
+
+static void *syscalls[] = {
+    [__NR_exit] = syscall_exit};
 
 int syscall_handler(struct itr_registers *registers)
 {
-  (void)registers;
+  int index = registers->eax;
+  uint32_t (*handler)(unsigned int, ...) = syscalls[index];
+  if (!handler)
+    return ITR_STOP;
+
+  memcpy(&sched_thread_get()->registers, registers, sizeof(struct itr_registers));
+  uint32_t result = handler(registers->ebx, registers->ecx, registers->edx, registers->esi, registers->edi);
+  registers->eax = result;
+  log_info("Syscall: Called syscall id = %d, result = 0x%x\n", index, result);
+
   return ITR_CONTINUE;
 }
 
