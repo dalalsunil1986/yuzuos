fee65a838a63598f81cc5fcd7467f1fd0e66e8b8
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-05-12 14:44:56 -0300

    Kernel: Added MIN_T, MAX_T on math.h

--- kernel/include/kernel/utils/math.h
+++ kernel/include/kernel/utils/math.h
@@ -2,4 +2,23 @@
 
 #define ALIGN_DOWN(base, size) ((base) & -((__typeof__(base))(size)))
 #define ALIGN_UP(base, size) ALIGN_DOWN((base) + (size)-1, (size))
-#define DIV_CEIL(x, y) ({__auto_type _x = (x); __auto_type _y = (y); _x / _y + (_x % _y > 0); })
+
+#define DIV_CEIL(x, y) (       \
+    {                          \
+      __auto_type _x = (x);    \
+      __auto_type _y = (y);    \
+      _x / _y + (_x % _y > 0); \
+    })
+
+#define MIN_T(type, x, y) (  \
+    {                        \
+      type __x = (x);        \
+      type __y = (y);        \
+      __x < __y ? __x : __y; \
+    })
+#define MAX_T(type, x, y) (  \
+    {                        \
+      type __x = (x);        \
+      type __y = (y);        \
+      __x > __y ? __x : __y; \
+    })
