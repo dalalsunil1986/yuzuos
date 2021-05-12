d3839030494133a34285d9eb8cb71bf438429e38
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-05-12 12:50:46 -0300

    Kernel: Added FMODE_ definitions on filesystem/virtual.h

--- kernel/include/kernel/filesystem/virtual.h
+++ kernel/include/kernel/filesystem/virtual.h
@@ -2,9 +2,40 @@
 
 #include <kernel/utils/dlist.h>
 #include <kernel/utils/types.h>
+#include <kernel/utils/fcntl.h>
 #include <stdint.h>
 
 #define VFS_BYTES_P_SECTOR 512
+
+#define FMODE_READ ((fmode_t)0x1)
+#define FMODE_WRITE ((fmode_t)0x2)
+#define FMODE_LSEEK ((fmode_t)0x4)
+#define FMODE_PREAD ((fmode_t)0x8)
+#define FMODE_PWRITE ((fmode_t)0x10)
+#define FMODE_EXEC ((fmode_t)0x20)
+#define FMODE_NDELAY ((fmode_t)0x40)
+#define FMODE_EXCL ((fmode_t)0x80)
+#define FMODE_WRITE_IOCTL ((fmode_t)0x100)
+#define FMODE_32BITHASH ((fmode_t)0x200)
+#define FMODE_64BITHASH ((fmode_t)0x400)
+#define FMODE_NOCMTIME ((fmode_t)0x800)
+#define FMODE_RANDOM ((fmode_t)0x1000)
+#define FMODE_UNSIGNED_OFFSET ((fmode_t)0x2000)
+#define FMODE_PATH ((fmode_t)0x4000)
+#define FMODE_ATOMIC_POS ((fmode_t)0x8000)
+#define FMODE_WRITER ((fmode_t)0x10000)
+#define FMODE_CAN_READ ((fmode_t)0x20000)
+#define FMODE_CAN_WRITE ((fmode_t)0x40000)
+#define FMODE_OPENED ((fmode_t)0x80000)
+#define FMODE_CREATED ((fmode_t)0x100000)
+#define FMODE_STREAM ((fmode_t)0x200000)
+#define FMODE_NONOTIFY ((fmode_t)0x4000000)
+#define FMODE_NOWAIT ((fmode_t)0x8000000)
+#define FMODE_NEED_UNMOUNT ((fmode_t)0x10000000)
+#define FMODE_NOACCOUNT ((fmode_t)0x20000000)
+#define FMODE_BUF_RASYNC ((fmode_t)0x40000000)
+
+#define OPEN_FMODE(flag) ((fmode_t)(((flag + 1) & O_ACCMODE) | (flag & FMODE_NONOTIFY)))
 
 struct vfs_dentry
 {
