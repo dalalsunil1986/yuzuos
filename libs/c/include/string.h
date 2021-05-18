f10b8f3e902f41f5120058d38cff3ad6a2734813
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-05-18 15:20:50 -0300

    LibC: Added strnlen

--- libs/c/include/string.h
+++ libs/c/include/string.h
@@ -6,4 +6,5 @@ void *memcpy(void *dest, const void *src, size_t n);
 void *memset(void *str, int c, size_t n);
 char *strcpy(char *, const char *);
 size_t strlen(const char *str);
+size_t strnlen(const char *s, size_t maxlen);
 char *strchr(const char *str, int c);
