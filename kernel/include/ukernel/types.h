49210367265c178659ebccab8c4f8a3f2717d39b
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-05-18 11:07:46 -0300

    Kernel: Added ukernel/types.h

---
+++ kernel/include/ukernel/types.h
@@ -0,0 +1,24 @@
+#pragma once
+
+typedef int pid_t;
+typedef int ssize_t;
+
+typedef long long off_t;
+typedef long long loff_t;
+
+typedef signed long time_t;
+typedef signed long blksize_t;
+typedef signed long blkcnt_t;
+
+typedef unsigned short umode_t;
+typedef unsigned short mode_t;
+
+typedef unsigned int fmode_t;
+typedef unsigned int gid_t;
+typedef unsigned int uid_t;
+typedef unsigned int nlink_t;
+typedef unsigned int dev_t;
+
+typedef unsigned long sector_t;
+
+typedef unsigned long long ino_t;
