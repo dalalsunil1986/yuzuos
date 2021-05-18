c327f60ba69e4a619430fa75a1ff847133af22d6
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-05-18 14:32:48 -0300

    LibC: Added memcpy, memset, strlen

--- libs/c/include/string.h
+++ libs/c/include/string.h
@@ -2,7 +2,7 @@
 
 #include <stddef.h>
 
-void *memcpy(void *, const void *, size_t);
-void *memset(void *, int, size_t);
+void *memcpy(void *dest, const void *src, size_t n);
+void *memset(void *str, int c, size_t n);
 char *strcpy(char *, const char *);
-size_t strlen(const char *);
+size_t strlen(const char *str);
---
+++ libs/c/string.c
@@ -0,0 +1,28 @@
+#include <string.h>
+
+void *memcpy(void *dest, const void *src, size_t n)
+{
+  char *p_dest = (char *)dest;
+  char *p_src = (char *)src;
+
+  for (size_t i = 0; i < n; i++)
+    p_dest[i] = p_src[i];
+
+  return dest;
+}
+
+void *memset(void *str, int c, size_t n)
+{
+  char *p_str = (char *)str;
+  for (size_t i = 0; i < n; i++)
+    p_str[i] = (unsigned char)c;
+  return str;
+}
+
+size_t strlen(const char *str)
+{
+  size_t len = 0;
+  while (str[len])
+    len++;
+  return len;
+}
