2a6854f8110c92869626dea6c59b22a2a0e82016
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-05-18 14:13:50 -0300

    LibUtils: Removed warnings on log_log

--- libs/utils/log.c
+++ libs/utils/log.c
@@ -8,6 +8,8 @@ static char buffer[LOG_BUF];
 _syscall2(log, const char *, int);
 void log_log(enum log_type type, const char *file, int line, const char *format, ...)
 {
+  (void)file;
+  (void)line;
   va_list ap;
   va_start(ap, format);
   int len = vsprintf(buffer, format, ap);
