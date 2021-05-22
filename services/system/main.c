02cfed4c444ac3af6220c8b2a234e3e2790fceb4
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-05-21 22:33:44 -0300

    System: Spawn programs from system.ini

--- services/system/main.c
+++ services/system/main.c
@@ -1,5 +1,29 @@
+#include <utils/log.h>
+#include <utils/ini.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+int system_ini_handler(const char *section, const char *key, const char *value)
+{
+  //FIXME support multiple keys from system.ini (use hashmap!?)
+  (void)section;
+  (void)key;
+
+  pid_t pid = fork();
+  if (!pid)
+  {
+    char *path = (char *)value;
+    char *const argv[] = {path, NULL};
+    execv(path, argv);
+  }
+
+  return INI_SUCCESS;
+}
+
 int main()
 {
-  while (1)
+  ini_open("/etc/system.ini", system_ini_handler);
+
+  while (true)
     ;
 }
