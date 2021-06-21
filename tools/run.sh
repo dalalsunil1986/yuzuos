a7e60b305d334ed93793987c9a3571413bbf4f14
Saullo Bretas Silva <saullo.silva303@gmail.com>
2021-06-21 17:04:44 -0300

    Tools: Fixed -cpu arg on run.sh

--- tools/run.sh
+++ tools/run.sh
@@ -16,9 +16,9 @@ fail() {
   $YUZUOS_QEMU_EXTRA_ARG 
   -kernel $YUZUOS_QEMU_KERNEL 
   -m $YUZUOS_QEMU_MEMORY
-  -cput $YUZUOS_QEMU_CPU
+  -cpu $YUZUOS_QEMU_CPU
   -serial stdio
   -drive file=${YUZUOS_QEMU_IMG},format=raw,index=0,media=disk"
 [ -e /dev/kvm ] && [ -r /dev/kvm ] && [ -w /dev/kvm ] && QEMU_KVM="-enable-kvm"
 
-$YUZUOS_QEMU_BIN $YUZUOS_QEMU_ARG $QEMU_KVM
+"$YUZUOS_QEMU_BIN" $YUZUOS_QEMU_ARG $QEMU_KVM
